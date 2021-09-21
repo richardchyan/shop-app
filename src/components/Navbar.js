2715955672f6c8bf704a5f1ef07ab8f67ca2ac14
Richard Chyan <33560310+richardchyan@users.noreply.github.com>
2021-09-21 16:00:05 -0400

    fixed sticky nav on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import { GrCart } from 'react-icons/gr';
 
 const Navbar = ({ toggleCartOpen }) => {
    return (
-      <div id='#' className="py-10 mb-8 bg-blue-800 rounded-lg">
-         <div className="flex justify-evenly p-2 space-x-3">
-            <h1 className="text-3xl md:text-6xl text-white uppercase track-wider border-4 border-white border-dotted md:w-2/3 md:m-auto py-4">John's Pawn Shop</h1>
+      <div id='#' className="sticky-nav md:static w-full py-8 mb-8 bg-blue-800 rounded-lg">
+         <div className="flex justify-center md:justify-evenly p-2 space-x-3">
+            <h1 className="text-3xl md:text-6xl text-white uppercase track-wider border-4 border-white border-dotted w-3/4 md:w-2/3 md:m-auto py-4">John's Pawn Shop</h1>
             <button onClick={toggleCartOpen} className="text-4xl bg-blue-200 rounded-lg p-2 md:hidden">
                <GrCart /> 
             </button>
